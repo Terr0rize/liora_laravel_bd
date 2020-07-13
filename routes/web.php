44e6bfc1e17b5d98c3f1736194bb2fcc5be3d632
ptkolenie@inbox.ru <57214464+Terr0rize@users.noreply.github.com>
2020-07-13 13:19:49 +0400

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+use App\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', "IndexController@Index");
+
+Route::get('/test', function()
+{
+    $users = User::get();
+    dd($users);
+});
